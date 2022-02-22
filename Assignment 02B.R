234aac620d03c7c5476a1f3dd1398ec328456bce
Aiden Forys <ajf225@lehigh.edu>
2022-02-22 07:54:20 -0500

    Assignment 02B.R

---
+++ Assignment 02B.R
@@ -0,0 +1,12 @@
+x <- 10:1
+y <- -4:5
+z <- c('Hockey', 'Football', 'Curling', 'Soccer', 'Rugby', 'Baseball', 'Golf', 'Basketball', 'Wrestling', 'Tennis')
+theDF <- data.frame(x,y,z)
+theDF
+str(theDF)
+theDF[1,2]
+View(theDF)
+theDF$x
+theDF$x[3]
+names(theDF)<-c("Popularity", "Team Strength", "Sport")
+sd(theDF$"Team Strength")
