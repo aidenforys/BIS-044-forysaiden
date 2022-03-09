20bad1ae8af668bd7d6c18acd97322c8e3dedcc9
Aiden Forys <ajf225@lehigh.edu>
2022-03-08 20:57:12 -0500

    Assignment 03.R

--- Assignment 03.R
+++ Assignment 03.R
@@ -1,4 +1,6 @@
-ptm <- proc.time()
-DF <- read.csv("Yellow_Sample.csv")
-CSV_READ_TIME <- (proc.time() - ptm)
-CSV_READ_TIME
+DF<-read.csv("Yellow_Sample.csv")
+DF<-DF[which(DF$trip_distance>0 & 
+               DF$trip_distance<10),]
+summary(DF$trip_distance)
+cat("The mean trip distance is", mean(DF$trip_distance), "miles")
+cat("The mean total amount is", mean(DF$total_amount), "usd")
