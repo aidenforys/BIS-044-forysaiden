5e576d1b4b7cd33b9f2260d38a9c624473a92e65
Carolyn Taylor <carolyntaylor11224@gmail.com>
2022-03-27 14:42:34 -0400

    assignment 2 grade

--- Assignment 02A.R
+++ Assignment 02A.R
@@ -1,1 +1,3 @@
 cat("Aiden, there are", as.double(as.Date("2022-5-6") - Sys.Date()), "days left in the semester.")
+
+#50/50
--- Assignment 03.R
+++ Assignment 03.R
@@ -4,3 +4,5 @@ DF<-DF[which(DF$trip_distance>0 &
 summary(DF$trip_distance)
 cat("The mean trip distance is", mean(DF$trip_distance), "miles")
 cat("The mean total amount is", mean(DF$total_amount), "usd")
+
+#-5 <= 10 95/100
